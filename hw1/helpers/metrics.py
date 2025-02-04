3cff6f9385f92f16c4bd94a1576ae0ff8e5a10da
Albert Lewis <alberttheperson@gmail.com>
2025-02-03 22:32:59 -0500

    started hw1 code, analytical linear regression and parts of metrics seem to be done

---
+++ hw1/helpers/metrics.py
@@ -0,0 +1,76 @@
+import numpy as np
+
+def compute_mse(predictions, targets):
+   """Compute Mean Squared Error between predictions and targets.
+   
+   Args:
+       predictions (np.ndarray): Model predictions
+       targets (np.ndarray): Target values
+       
+   Returns:
+       float: MSE value
+   """
+
+   return ((targets.T - predictions)**2).mean()
+
+def compute_rmse(predictions, targets): 
+   """Compute Root Mean Squared Error between predictions and targets.
+   
+   Args:
+       predictions (np.ndarray): Model predictions
+       targets (np.ndarray): Target values
+       
+   Returns:
+       float: RMSE value
+   """
+   pass
+
+def compute_mae(predictions, targets):
+   """Compute Mean Absolute Error between predictions and targets.
+   
+   Args:
+       predictions (np.ndarray): Model predictions  
+       targets (np.ndarray): Target values
+       
+   Returns:
+       float: MAE value
+   """
+   pass
+
+def compute_position_error(predictions, targets):
+   """Compute mean Euclidean error for position predictions (x,y,z).
+   
+   Args:
+       predictions (np.ndarray): Model predictions with position in first 3 columns
+       targets (np.ndarray): Target values with position in first 3 columns
+       
+   Returns:
+       float: Mean position error
+   """
+   print(predictions.shape)
+   print(targets.shape)
+   diff = (predictions[:3, :] - targets.T[:3, :])
+
+   sum = 0
+   for col in diff:
+      sum += (col[0]**2 + col[1]**2 + col[2]**2)**0.5
+
+   return sum / diff.shape[1]
+   
+def compute_rotation_error(predictions, targets):
+   """Compute mean Euclidean error for rotation predictions (rx,ry,rz).
+   
+   Args:
+       predictions (np.ndarray): Model predictions with rotation in last 3 columns
+       targets (np.ndarray): Target values with rotation in last 3 columns
+       
+   Returns:
+       float: Mean rotation error
+   """
+   diff = (predictions[4:6, :] - targets.T[4:6, :])
+
+   sum = 0
+   for col in diff:
+      sum += (col[0]**2 + col[1]**2 + col[2]**2)**0.5
+
+   return sum / diff.shape[1]
